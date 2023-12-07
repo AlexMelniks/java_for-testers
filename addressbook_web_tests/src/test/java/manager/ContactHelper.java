beaeead25d49df6895d70d0fa1faa2c584d01597
Malex <malexx2996@gmail.com>
2023-12-08 01:46:08 +0300

    Задание №16

--- addressbook_web_tests/src/test/java/manager/ContactHelper.java
+++ addressbook_web_tests/src/test/java/manager/ContactHelper.java
@@ -3,14 +3,11 @@ package manager;
 import model.ContactData;
 import model.GroupData;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class ContactHelper extends HelperBase {
     public ContactHelper(ApplicationManager manager) {
@@ -187,46 +184,16 @@ public class ContactHelper extends HelperBase {
         return contacts;
     }
 
-    public String getPhones(ContactData contact) {
-        return manager.driver.findElement(By.xpath(
-                String.format("//input[@id='%s']/../../td[6]", contact.id()))).getText();
+    public ArrayList<String> getContactInfo(ContactData contact) {
+        var result = new ArrayList<String>();
+         WebElement row = manager.driver.findElement(By.xpath(String.format("//input[@id='%s']/../..", contact.id())));
+            result.add(row.findElements(By.tagName("td")).get(3).getText());
+            result.add(row.findElements(By.tagName("td")).get(4).getText());
+            result.add(row.findElements(By.tagName("td")).get(5).getText());
 
-    }
-
-    public Map<String, String> getPhones() {
-        var result = new HashMap<String, String>();
-        List<WebElement> rows = manager.driver.findElements(By.name("entry"));
-        for (WebElement row : rows) {
-            var id = row.findElement(By.tagName("input")).getAttribute("id");
-            var phones = row.findElements(By.tagName("td")).get(5).getText();
-            var email = row.findElements(By.tagName("td")).get(4).getText();
-            var address = row.findElements(By.tagName("td")).get(3).getText();
-            result.put(id, phones);
-        }
         return result;
     }
 
-    public Map<String, String> getEmail() {
-        var result = new HashMap<String, String>();
-        List<WebElement> rows = manager.driver.findElements(By.name("entry"));
-        for (WebElement row : rows) {
-            var id = row.findElement(By.tagName("input")).getAttribute("id");
-            var email = row.findElements(By.tagName("td")).get(4).getText();
-            result.put(id, email);
-        }
-        return result;
-    }
-
-    public Map<String, String> getAddress() {
-        var result = new HashMap<String, String>();
-        List<WebElement> rows = manager.driver.findElements(By.name("entry"));
-        for (WebElement row : rows) {
-            var id = row.findElement(By.tagName("input")).getAttribute("id");
-            var address = row.findElements(By.tagName("td")).get(3).getText();
-            result.put(id, address);
-        }
-        return result;
-    }
     private void selectGroup(GroupData group) {
         new Select(manager.driver.findElement(By.name("new_group"))).selectByValue(group.id());
     }
--- addressbook_web_tests/src/test/java/tests/contact/ContactInfoTests.java
+++ addressbook_web_tests/src/test/java/tests/contact/ContactInfoTests.java
@@ -1,58 +1,39 @@
-package tests;
+package tests.contact;
 
 import model.ContactData;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import tests.base.TestBase;
 
+import java.util.ArrayList;
+import java.util.Random;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class ContactInfoTests extends TestBase {
 
     @Test
-    void testPhones() {
+    void testContactInfo() {
         if (app.hbm().getContactCount() == 0) {
             app.hbm().createContact(new ContactData());
         }
         var contacts = app.hbm().getContactList();
-        var phones = app.contacts().getPhones();
-        for (var contact : contacts) {
-            var expected = Stream.of(contact.home(), contact.mobile(), contact.work(), contact.phone2())
+        var rnd = new Random();
+        var index = rnd.nextInt(contacts.size());
+        var contact = new ContactData();
+        var contactInfo = app.contacts().getContactInfo(contacts.get(index));
+        var expected = new ArrayList<String>();
+        expected.add(Stream.of(contact.address())
+                .filter(s -> s != null && !"".equals(s))
+                .collect(Collectors.joining("\n")));
+        expected.add(Stream.of(contact.email(), contact.email2(), contact.email3())
+                .filter(s -> s != null && !"".equals(s))
+                .collect(Collectors.joining("\n")));
+        expected.add(Stream.of(contact.home(), contact.mobile(), contact.work(), contact.phone2())
                     .filter(s -> s != null && !"".equals(s))
-                    .collect(Collectors.joining("\n"));
-            Assertions.assertEquals(expected, phones.get(contact.id()));
-        }
-    }
-
-    @Test
-    void testEmail() {
-        if (app.hbm().getContactCount() == 0) {
-            app.hbm().createContact(new ContactData());
-        }
-        var contacts = app.hbm().getContactList();
-        var email = app.contacts().getEmail();
-        for (var contact : contacts) {
-            var expected = Stream.of(contact.email(), contact.email2(), contact.email3())
-                    .filter(s -> s != null && !"".equals(s))
-                    .collect(Collectors.joining("\n"));
-            Assertions.assertEquals(expected, email.get(contact.id()));
-        }
-    }
-
-    @Test
-    void testAddress() {
-        if (app.hbm().getContactCount() == 0) {
-            app.hbm().createContact(new ContactData());
-        }
-        var contacts = app.hbm().getContactList();
-        var address = app.contacts().getAddress();
-        for (var contact : contacts) {
-            var expected = Stream.of(contact.address())
-                    .filter(s -> s != null && !"".equals(s))
-                    .collect(Collectors.joining("\n"));
-            Assertions.assertEquals(expected, address.get(contact.id()));
-        }
+                    .collect(Collectors.joining("\n")));
+        Assertions.assertEquals(expected, contactInfo);
     }
 }
+
 
